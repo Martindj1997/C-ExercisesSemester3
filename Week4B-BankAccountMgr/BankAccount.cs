aa350795e31f0b34a5c0fb5ee9308e43a366e04e
Joe <Joe>
2020-10-02 13:48:25 -0400

    First half of bank account exercise.

---
+++ Week4B-BankAccountMgr/BankAccount.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4B_BankAccountMgr
+{
+    class BankAccount
+    {
+        private static int number = 1000;
+        public decimal Balance { get; private set; }
+        private int Number { get; set; }
+        private Person Owner { get; set; }
+        public List<Transaction> Transactions { get; private set; }
+
+        public BankAccount(Person owner)
+        {
+            Number = number++;
+            Owner = owner;
+            Transactions = new List<Transaction>();
+        }
+
+        public Transaction Deposit(decimal amount)
+        {
+            Transaction transaction = new Transaction(amount, TransactionType.DEPOSIT);
+            Transactions.Add(transaction);
+            Balance += amount;
+            return transaction;
+        }
+
+        public Transaction Withdraw(decimal amount)
+        {
+            Transaction transaction = new Transaction(amount, TransactionType.WITHDRAWAL);
+            Transactions.Add(transaction);
+            Balance -= amount;
+            return transaction;
+        }
+
+        public override string ToString()
+        {
+            return $"{Number} {Owner} {Balance}";
+        }
+
+    }
+}
---
+++ Week4B-BankAccountMgr/Transaction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4B_BankAccountMgr
+{
+    public enum TransactionType { DEPOSIT, WITHDRAWAL};
+    public class Transaction
+    {
+        public Transaction(decimal amount, TransactionType type)
+        {
+            Amount = amount;
+            Type = type;
+            Timestamp = DateTime.Now;
+        }
+
+        public decimal Amount { get; set; }
+        public DateTime Timestamp { get; set; }
+        public TransactionType Type { get; set; }
+        public override string ToString()
+        {
+            return $"{Timestamp} {Type} {Amount}"; 
+        }
+    }
+}
