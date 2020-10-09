449c2f37ce1be3b732858af16df30b4f0ce2fdbe
Joe <Joe>
2020-10-09 14:13:48 -0400

    First half of listbox exercise in Windows Forms.

---
+++ Week5B/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Week5B
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nameTB = new System.Windows.Forms.TextBox();
+            this.namesLB = new System.Windows.Forms.ListBox();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.moveUpBtn = new System.Windows.Forms.Button();
+            this.moveDownBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.resultLbl = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 13);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(379, 420);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.resultLbl);
+            this.tabPage1.Controls.Add(this.clearBtn);
+            this.tabPage1.Controls.Add(this.moveDownBtn);
+            this.tabPage1.Controls.Add(this.moveUpBtn);
+            this.tabPage1.Controls.Add(this.deleteBtn);
+            this.tabPage1.Controls.Add(this.addBtn);
+            this.tabPage1.Controls.Add(this.namesLB);
+            this.tabPage1.Controls.Add(this.nameTB);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(371, 387);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Exercise 5";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(371, 387);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "ComboBoxes";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name:";
+            // 
+            // nameTB
+            // 
+            this.nameTB.Location = new System.Drawing.Point(92, 46);
+            this.nameTB.Name = "nameTB";
+            this.nameTB.Size = new System.Drawing.Size(125, 26);
+            this.nameTB.TabIndex = 1;
+            // 
+            // namesLB
+            // 
+            this.namesLB.FormattingEnabled = true;
+            this.namesLB.ItemHeight = 20;
+            this.namesLB.Location = new System.Drawing.Point(35, 107);
+            this.namesLB.Name = "namesLB";
+            this.namesLB.Size = new System.Drawing.Size(182, 224);
+            this.namesLB.TabIndex = 2;
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(239, 47);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(102, 30);
+            this.addBtn.TabIndex = 3;
+            this.addBtn.Text = "Add";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.Location = new System.Drawing.Point(239, 102);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(102, 30);
+            this.deleteBtn.TabIndex = 4;
+            this.deleteBtn.Text = "Delete";
+            this.deleteBtn.UseVisualStyleBackColor = true;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
+            // 
+            // moveUpBtn
+            // 
+            this.moveUpBtn.Location = new System.Drawing.Point(239, 159);
+            this.moveUpBtn.Name = "moveUpBtn";
+            this.moveUpBtn.Size = new System.Drawing.Size(102, 30);
+            this.moveUpBtn.TabIndex = 5;
+            this.moveUpBtn.Text = "Move Up";
+            this.moveUpBtn.UseVisualStyleBackColor = true;
+            this.moveUpBtn.Click += new System.EventHandler(this.moveUpBtn_Click);
+            // 
+            // moveDownBtn
+            // 
+            this.moveDownBtn.Location = new System.Drawing.Point(239, 212);
+            this.moveDownBtn.Name = "moveDownBtn";
+            this.moveDownBtn.Size = new System.Drawing.Size(102, 30);
+            this.moveDownBtn.TabIndex = 6;
+            this.moveDownBtn.Text = "Move Down";
+            this.moveDownBtn.UseVisualStyleBackColor = true;
+            this.moveDownBtn.Click += new System.EventHandler(this.moveDownBtn_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(239, 264);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(102, 30);
+            this.clearBtn.TabIndex = 7;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // resultLbl
+            // 
+            this.resultLbl.AutoSize = true;
+            this.resultLbl.Location = new System.Drawing.Point(35, 359);
+            this.resultLbl.Name = "resultLbl";
+            this.resultLbl.Size = new System.Drawing.Size(0, 20);
+            this.resultLbl.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 444);
+            this.Controls.Add(this.tabControl1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label resultLbl;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.Button moveDownBtn;
+        private System.Windows.Forms.Button moveUpBtn;
+        private System.Windows.Forms.Button deleteBtn;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.ListBox namesLB;
+        private System.Windows.Forms.TextBox nameTB;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Week5B/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week5B
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            string name = nameTB.Text.Trim();
+            if (name.Length < 1)
+            {
+                OutputResult(Color.Red, "Add name failed!  Name length must be at least 1 character");
+                return;
+            }
+            namesLB.Items.Add(name);
+            nameTB.Clear();
+            OutputResult(Color.Green, "New name added successfully.");
+        }
+
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+            if (namesLB.SelectedIndex == -1)
+            {
+                OutputResult(Color.Red, "Delete name failed!  No name is currently selected in the list.");
+                return;
+            }
+            namesLB.Items.RemoveAt(namesLB.SelectedIndex);
+            OutputResult(Color.Green, "Name deleted successfully.");
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            if (namesLB.Items.Count > 0)
+            {
+                //MessageBox.Show()
+                namesLB.Items.Clear();
+                OutputResult(Color.Green, "Name list cleared.");
+            }
+        }
+
+        private void moveUpBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void moveDownBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OutputResult(Color color, string message)
+        {
+            resultLbl.Text = message;
+            resultLbl.ForeColor = color;
+        }
+
+    }
+}
