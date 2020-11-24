007d240920c7d60214a06182b6c72b74ffb61454
Joe <Joe>
2020-11-24 14:04:30 -0500

    Generic repository using interface; simple datagridview project for cars database.

---
+++ Week11-GenericCRUD/PersonSet.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week11_GenericCRUD
+{
+    class PersonSet : IRepository<Person>
+    {
+        private List<Person> People { get; set; }
+
+        public void Create(Person value)
+        {
+            People.Add(value);
+        }
+
+        public Person Read(int id)
+        {
+            int index = People.FindIndex(p => p.Id == id);
+            if (index != -1)
+                return People[index];
+            return null;
+        }
+
+        public void Update(Person value)
+        {
+            int index = People.FindIndex(p => p.Id == value.Id);
+            if (index != -1)
+                People[index] = value;
+        }
+
+        public void Delete(int id)
+        {
+            int index = People.FindIndex(p => p.Id == id);
+            if (index != -1)
+                People.RemoveAt(index);
+        }
+
+    }
+}
---
+++ Week11DB/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿
+namespace Week11DB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dN20_CarsDataSet = new Week11DB.DN20_CarsDataSet();
+            this.cARSBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.cARSTableAdapter = new Week11DB.DN20_CarsDataSetTableAdapters.CARSTableAdapter();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.makeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.modelDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.modelyearDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.mILEAGEBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.mILEAGETableAdapter = new Week11DB.DN20_CarsDataSetTableAdapters.MILEAGETableAdapter();
+            this.idDataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.startkmDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.endkmDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.gasusedDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.caridDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dN20_CarsDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cARSBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mILEAGEBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.makeDataGridViewTextBoxColumn,
+            this.modelDataGridViewTextBoxColumn,
+            this.modelyearDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.cARSBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(69, 45);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(631, 152);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dN20_CarsDataSet
+            // 
+            this.dN20_CarsDataSet.DataSetName = "DN20_CarsDataSet";
+            this.dN20_CarsDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // cARSBindingSource
+            // 
+            this.cARSBindingSource.DataMember = "CARS";
+            this.cARSBindingSource.DataSource = this.dN20_CarsDataSet;
+            // 
+            // cARSTableAdapter
+            // 
+            this.cARSTableAdapter.ClearBeforeFill = true;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // makeDataGridViewTextBoxColumn
+            // 
+            this.makeDataGridViewTextBoxColumn.DataPropertyName = "make";
+            this.makeDataGridViewTextBoxColumn.HeaderText = "make";
+            this.makeDataGridViewTextBoxColumn.Name = "makeDataGridViewTextBoxColumn";
+            // 
+            // modelDataGridViewTextBoxColumn
+            // 
+            this.modelDataGridViewTextBoxColumn.DataPropertyName = "model";
+            this.modelDataGridViewTextBoxColumn.HeaderText = "model";
+            this.modelDataGridViewTextBoxColumn.Name = "modelDataGridViewTextBoxColumn";
+            // 
+            // modelyearDataGridViewTextBoxColumn
+            // 
+            this.modelyearDataGridViewTextBoxColumn.DataPropertyName = "modelyear";
+            this.modelyearDataGridViewTextBoxColumn.HeaderText = "modelyear";
+            this.modelyearDataGridViewTextBoxColumn.Name = "modelyearDataGridViewTextBoxColumn";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AutoGenerateColumns = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn1,
+            this.startkmDataGridViewTextBoxColumn,
+            this.endkmDataGridViewTextBoxColumn,
+            this.gasusedDataGridViewTextBoxColumn,
+            this.caridDataGridViewTextBoxColumn});
+            this.dataGridView2.DataSource = this.mILEAGEBindingSource;
+            this.dataGridView2.Location = new System.Drawing.Point(69, 241);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(631, 150);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // mILEAGEBindingSource
+            // 
+            this.mILEAGEBindingSource.DataMember = "MILEAGE";
+            this.mILEAGEBindingSource.DataSource = this.dN20_CarsDataSet;
+            // 
+            // mILEAGETableAdapter
+            // 
+            this.mILEAGETableAdapter.ClearBeforeFill = true;
+            // 
+            // idDataGridViewTextBoxColumn1
+            // 
+            this.idDataGridViewTextBoxColumn1.DataPropertyName = "id";
+            this.idDataGridViewTextBoxColumn1.HeaderText = "id";
+            this.idDataGridViewTextBoxColumn1.Name = "idDataGridViewTextBoxColumn1";
+            this.idDataGridViewTextBoxColumn1.ReadOnly = true;
+            // 
+            // startkmDataGridViewTextBoxColumn
+            // 
+            this.startkmDataGridViewTextBoxColumn.DataPropertyName = "startkm";
+            this.startkmDataGridViewTextBoxColumn.HeaderText = "startkm";
+            this.startkmDataGridViewTextBoxColumn.Name = "startkmDataGridViewTextBoxColumn";
+            // 
+            // endkmDataGridViewTextBoxColumn
+            // 
+            this.endkmDataGridViewTextBoxColumn.DataPropertyName = "endkm";
+            this.endkmDataGridViewTextBoxColumn.HeaderText = "endkm";
+            this.endkmDataGridViewTextBoxColumn.Name = "endkmDataGridViewTextBoxColumn";
+            // 
+            // gasusedDataGridViewTextBoxColumn
+            // 
+            this.gasusedDataGridViewTextBoxColumn.DataPropertyName = "gasused";
+            this.gasusedDataGridViewTextBoxColumn.HeaderText = "gasused";
+            this.gasusedDataGridViewTextBoxColumn.Name = "gasusedDataGridViewTextBoxColumn";
+            // 
+            // caridDataGridViewTextBoxColumn
+            // 
+            this.caridDataGridViewTextBoxColumn.DataPropertyName = "car_id";
+            this.caridDataGridViewTextBoxColumn.HeaderText = "car_id";
+            this.caridDataGridViewTextBoxColumn.Name = "caridDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dN20_CarsDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cARSBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mILEAGEBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private DN20_CarsDataSet dN20_CarsDataSet;
+        private System.Windows.Forms.BindingSource cARSBindingSource;
+        private DN20_CarsDataSetTableAdapters.CARSTableAdapter cARSTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn makeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn modelDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn modelyearDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.BindingSource mILEAGEBindingSource;
+        private DN20_CarsDataSetTableAdapters.MILEAGETableAdapter mILEAGETableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn startkmDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn endkmDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn gasusedDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn caridDataGridViewTextBoxColumn;
+    }
+}
+
