66764ea10683b47dc4b5f7ad153cc2aec2fa5650
Joe <Joe>
2020-12-01 14:17:44 -0500

    Week 12 database exercise added.

---
+++ Week11DBExercise/MainForm.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace Week12DBExercise
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listCarsButton = new System.Windows.Forms.Button();
+            this.resultsListBox = new System.Windows.Forms.ListBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.vehicleTB = new System.Windows.Forms.TextBox();
+            this.mileageTB = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listCarsButton
+            // 
+            this.listCarsButton.Location = new System.Drawing.Point(54, 57);
+            this.listCarsButton.Name = "listCarsButton";
+            this.listCarsButton.Size = new System.Drawing.Size(142, 44);
+            this.listCarsButton.TabIndex = 0;
+            this.listCarsButton.Text = "Get List of Cars";
+            this.listCarsButton.UseVisualStyleBackColor = true;
+            this.listCarsButton.Click += new System.EventHandler(this.listCarsButton_Click);
+            // 
+            // resultsListBox
+            // 
+            this.resultsListBox.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultsListBox.FormattingEnabled = true;
+            this.resultsListBox.ItemHeight = 18;
+            this.resultsListBox.Location = new System.Drawing.Point(54, 149);
+            this.resultsListBox.Name = "resultsListBox";
+            this.resultsListBox.Size = new System.Drawing.Size(535, 274);
+            this.resultsListBox.TabIndex = 1;
+            this.resultsListBox.SelectedIndexChanged += new System.EventHandler(this.resultsListBox_SelectedIndexChanged);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 458);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1200, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(23, 17);
+            this.statusLabel.Text = "OK";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(595, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Selected Vehicle:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(596, 216);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Average Mileage:";
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(652, 256);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(142, 44);
+            this.addBtn.TabIndex = 5;
+            this.addBtn.Text = "Add Car";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // vehicleTB
+            // 
+            this.vehicleTB.Location = new System.Drawing.Point(733, 157);
+            this.vehicleTB.Name = "vehicleTB";
+            this.vehicleTB.ReadOnly = true;
+            this.vehicleTB.Size = new System.Drawing.Size(212, 26);
+            this.vehicleTB.TabIndex = 6;
+            // 
+            // mileageTB
+            // 
+            this.mileageTB.Location = new System.Drawing.Point(733, 213);
+            this.mileageTB.Name = "mileageTB";
+            this.mileageTB.ReadOnly = true;
+            this.mileageTB.Size = new System.Drawing.Size(100, 26);
+            this.mileageTB.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(848, 216);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "L/100km";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 480);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.mileageTB);
+            this.Controls.Add(this.vehicleTB);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.resultsListBox);
+            this.Controls.Add(this.listCarsButton);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button listCarsButton;
+        private System.Windows.Forms.ListBox resultsListBox;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statusLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.TextBox vehicleTB;
+        private System.Windows.Forms.TextBox mileageTB;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Week11DBExercise/MainForm.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Week12DBExercise
+{
+    public partial class MainForm : Form
+    {
+        public SqlConnection Connection { get; }
+        String connectionString = @"Data Source=.;Initial " +
+        "Catalog=COMP10204ExerciseWeek12;Integrated Security=True";
+        public MainForm()
+        {
+            InitializeComponent();
+            try
+            {
+                Connection = new SqlConnection();
+                Connection.ConnectionString = connectionString;
+                Connection.Open();
+                statusLabel.Text = "Connected to Database Successfully";
+            }
+            catch (Exception ex)
+            {
+                statusLabel.Text = "Database Connection failed - check Connection String : " +
+                ex.Message;
+            }
+        }
+
+        private void listCarsButton_Click(object sender, EventArgs e)
+        {
+            resultsListBox.Items.Clear();
+            try
+            {
+                SqlCommand command = new SqlCommand("SELECT * FROM Cars", Connection);
+                // Create new SqlDataReader object and read data from the command.
+                SqlDataReader reader = command.ExecuteReader();
+                // while there is another record present
+                while (reader.Read())
+                {
+                    // write the data on to the screen
+                    string output = "";
+                    for (int i = 0; i < reader.FieldCount; i++)
+                        output += String.Format("{0,-10} | ", reader[i]);
+                    resultsListBox.Items.Add(output);
+                }
+                statusLabel.Text = "Database Select Success";
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                statusLabel.Text = "Database operation failed: " + ex.Message;
+            }
+        }
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            AddCarForm form = new AddCarForm(this); // Pass Reference to this class for other info
+            form.ShowDialog(this);
+            // Update the form with any changes to the DB
+            listCarsButton_Click(this, null);
+        }
+
+        private void resultsListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Mileage WHERE car_id = @carId", Connection);
+                SqlParameter param = new SqlParameter("@carId", SqlDbType.Int);
+                cmd.Parameters.Add(param);
+
+                string[] carFields = resultsListBox.SelectedItem.ToString().Split('|');
+                bool result = int.TryParse(carFields[0].Trim(), out int carId);
+                if (result)
+                {
+                    // update the parameter value
+                    param.Value = carId;
+
+                    // run select query to get all mileage entries for the car in question
+
+                    // calculate the mileage
+                }
+
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
